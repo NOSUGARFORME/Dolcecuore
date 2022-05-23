2538b64fafa3c59bd5affa66d49aef59b7b424eb
Ivan Bolsun <ivanbolsun03@gmail.com>
2022-05-23 21:50:10 +0300

    (#6) migrate to .NET 6

--- src/Services.Basket/Dolcecuore.Services.Basket.Api/Program.cs
+++ src/Services.Basket/Dolcecuore.Services.Basket.Api/Program.cs
@@ -1,20 +1,64 @@
-using Microsoft.AspNetCore.Hosting;
+using System;
+using Dolcecuore.Services.Basket.Api.GrpcServices;
+using Dolcecuore.Services.Basket.Api.Repositories;
+using Dolcecuore.Services.Basket.Api.Repositories.Interfaces;
+using Dolcecuore.Services.Discount.Grpc.Protos;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
-namespace Dolcecuore.Services.Basket.Api
+// namespace Dolcecuore.Services.Basket.Api
+// {
+//     public class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             CreateHostBuilder(args).Build().Run();
+//         }
+//
+//         public static IHostBuilder CreateHostBuilder(string[] args) =>
+//             Host.CreateDefaultBuilder(args)
+//                 .ConfigureWebHostDefaults(webBuilder =>
+//                 {
+//                     webBuilder.UseStartup<Startup>();
+//                 });
+//     }
+// }
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddStackExchangeRedisCache(opts =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    opts.Configuration = builder.Configuration["Redis:ConnectionString"];
+});
+            
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
+    opts => opts.Address = new Uri(builder.Configuration["Grpc:DiscountUrl"]));
+builder.Services.AddScoped<DiscountGrpcService>();
+            
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Dolcecuore.Services.Basket.Api", Version = "v1" });
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dolcecuore.Services.Basket.Api v1"));
 }
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
